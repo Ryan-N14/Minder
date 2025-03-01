bed2678f40c15833d4cde53d2889e3bb4115aad7
Ryan-N14 <141654254+Ryan-N14@users.noreply.github.com>
2025-03-01 02:23:33 -0600

    gave login form ids

--- static/js/auth.js
+++ static/js/auth.js
@@ -50,7 +50,7 @@ async function signUp() {
   const confirmPassword = document.getElementById("confirm-password").value;
 
   // calling end point to communicate with backend (POST)
-  const response = await fetch("http://127.0.0.1:5000/signup", {
+  const response = await fetch("http://127.0.0.1:5000/auth/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, confirmPassword }),
